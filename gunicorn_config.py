cc28dc1b4c69809f47303f55423ac2f3fc73498b
lukecz1 <luke@czachor.us>
2024-08-05 11:37:33 -0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,5 @@
-bind = "0.0.0.0:80"
-workers = 2
+# gunicorn_config.py
+bind = "0.0.0.0:8000"  # Bind to all IP addresses on port 8000
+workers = 2            # Number of worker processes
+threads = 4            # Number of threads per worker
+timeout = 120          # Timeout for requests
